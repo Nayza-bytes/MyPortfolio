6f22f551608cca0fa4ecf084c281b1ed7e0cf3bb
Nayza-Bytes <quetru123456789@gmail.com>
2024-09-27 14:31:10 +0200

    Recoded Jome section to be responsive + added a development warning

--- src/app/Header.js
+++ src/app/Header.js
@@ -2,14 +2,17 @@ const Header = () => {
     const linkStyle = "p-8 no-underline transition ease-in-out duration-500 hover:text-orange_primary hover:underline";
 
     return (
-        <div class="top-0 left-0 w-screen h-16 m-0 flex flex-row bg-bg_gray-100 text-text_color-100">
-            <div className="flex justify-center items-center pl-32">
-                <a class={linkStyle} href="#home">Home</a>
-                <a class={linkStyle} href="#about">About me</a>
-                <a class={linkStyle} href="#projects">Project</a>
-                <a class={linkStyle} href="#contact">Contact</a>
+            <div class="top-0 left-0 w-screen h-16 m-0 flex flex-row bg-bg_gray-100 text-text_color-100">
+                <div className="w-screen h-11 absolute bg-red_secondary-200 flex items-center justify-center" >
+                    <p>The website is still in development, not fully responsive yet</p>
+                </div>
+                <div className="flex justify-center items-center pl-32 mt-20">
+                    <a class={linkStyle} href="#home">Home</a>
+                    <a class={linkStyle} href="#about">About me</a>
+                    <a class={linkStyle} href="#projects">Project</a>
+                    <a class={linkStyle} href="#contact">Contact</a>
+                </div>
             </div>
-        </div>
     )
 };
 
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,20 +1,20 @@
 const Home = () => {
     
     return (
-        <div className="flex items-center bg-bg_gray-100 text-text_color-100 w-screen h-auto 2xl:pl-40 pl-9 overflow-hidden" id="home">
+        <div className="flex items-center bg-bg_gray-100 text-text_color-100 w-screen h-auto lg:pl-40 pl-9 overflow-hidden" id="home">
             <div className="mt-48 mb-48">
                 <div className="flex items-center">
-                    <div className="w-64 h-11 bg-orange_primary"></div>
-                    <h1 className="text-8xl xl:text-6xl font-black mb-6 ml-6">Hey, I'm Ilian</h1>
+                    <div className="lg:block hidden w-64 h-11 bg-orange_primary"></div>
+                    <h1 className="2xl:text-8xl lg:text-6xl text-3xl font-black mb-6 ml-6">Hey, I'm Ilian</h1>
                 </div>
-                <h1 className="text-8xl font-black">A Young Developer and </h1>
-                <div className="flex items-center mt-6">
-                    <h1 className="text-8xl font-black">Copywriter.</h1>
-                    <div className="w-256 h-11 bg-orange_primary"></div>
+                <h1 className="2xl:text-8xl lg:text-6xl text-3xl font-black ml-14 lg:ml-0">A Young Developer and </h1>
+                <div className="flex flex-col lg:flex-row lg:items-center mt-6">
+                    <h1 className="2xl:text-8xl lg:text-6xl text-3xl font-black ml-14 lg:ml-0">Copywriter.</h1>
+                    <div className="2xl:w-256 lg:h-11 xl:w-192 lg:w-128 w-32 h-3 lg:mt-0 mt-4 ml-6 bg-orange_primary"></div>
                 </div>
-                <div className="flex justify-center items-center mt-24">
+                <div className="flex justify-center items-center flex-col md:flex-row mt-24">
                     <a href="#contact" className="text-2xl underline transition ease-in-out duration-500 hover:text-orange_primary hover:underline mr-32">Get in touch</a>
-                    <a href="#projects" className="text-xl text-text_color-200 underline transition ease-in-out duration-500 hover:text-orange_primary hover:underline ">See my projects</a>
+                    <a href="#projects" className="text-xl mt-7 md:mt-0 text-text_color-200 underline transition ease-in-out duration-500 hover:text-orange_primary hover:underline ">See my projects</a>
                 </div>
             </div>
         </div>
